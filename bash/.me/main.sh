fe6f018ea925d1a3f70bf18c341db1e83de6a41a
an9wer <an9wer@gmail.com>
2018-04-26 20:21:19 +0800

    Add some basic aliases

--- bash/.me/main.sh
+++ bash/.me/main.sh
@@ -37,6 +37,13 @@ unset BOLD BLUE RED END
 
 # aliases
 # -----------------------------------------------------------------------------
+alias ls="ls --color=auto"
+alias ll="ls -l"
+
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+
 alias vimv="vim ~/.vimrc"
 alias vimb="vim ~/.bashrc"
 alias vimt="vim ~/.tmux.conf"
