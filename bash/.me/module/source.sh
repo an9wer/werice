163c45f02f8cb6dcb15a5c9d5c58fdfc62b5ab45
an9wer <an9wer@gmail.com>
2018-05-07 18:19:36 +0800

    Add module source

---
+++ bash/.me/module/source.sh
@@ -0,0 +1,25 @@
+# usage
+# -----------------------------------------------------------------------------
+source() {
+  # python virtual environment prompt
+  if [[ $* =~ "/bin/activate" ]]; then
+    # disable python default virtual enable prompt used by (venv)/bin/activate
+    VIRTUAL_ENV_DISABLE_PROMPT=1
+
+    if builtin source "$*"; then
+      # record old virtual ps1 used by (venv)/bin/activate
+      _OLD_VIRTUAL_PS1="$PS1"
+
+      local path=$(readlink -ev "$*")
+      local venv="${path%%/bin/activate}"
+      local BOLD="\[\e[1m\]" RED="\[\e[91m\]" GREEN="\[\e[92m\]"
+      local BLUE="\[\e[94m\]" YELLOW="\[\e[93m\]" END="\[\e[0m\]"
+      PS1="${YELLOW}┏─━ ${END}($venv) ${BOLD}${GREEN}\u@\h${END} "
+      PS1+="at ${BOLD}${RED}\t${END} in ${BOLD}${BLUE}\w${END}\n"
+      PS1+="${YELLOW}┗─━ \$${END} "
+      export PS1
+    fi
+  else
+    builtin source "$*"
+  fi
+}
