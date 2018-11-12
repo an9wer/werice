c95e5e08ec819c0adc51fae0c7f43dca674a01e9
an9wer <an9wer@gmail.com>
2018-11-12 17:18:11 +0800

    Bash: start ssh agent automatically

--- bash/.me/module/ssh.sh
+++ bash/.me/module/ssh.sh
@@ -7,6 +7,13 @@ me_unset_ssh() {
 
 # usage
 # -----------------------------------------------------------------------------
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+  ssh-agent > ~/.ssh-agent.rc
+fi
+if [[ "$SSH_AGENT_PID" == "" ]]; then
+  eval "$(<~/.ssh-agent.rc)"
+fi
+
 sshh() {
   grep -wi "Host" ~/.ssh/config | grep -wv "\*" | sed 's/Host//'
 }
