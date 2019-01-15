e917e42e631c8198a738f9ef83da02f724291f41
an9wer <an9wer@gmail.com>
2019-01-15 23:49:45 +0800

    Condition to ssh-agent

--- .me/pkg/ssh.sh
+++ .me/pkg/ssh.sh
@@ -7,12 +7,17 @@ me_unset_ssh() {
 
 # The hack way
 # -----------------------------------------------------------------------------
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-  ssh-agent > ~/.ssh/.ssh-agent.rc
-  eval "$(<~/.ssh/.ssh-agent.rc)" > /dev/null
-fi
-if [[ "$SSH_AGENT_PID" == "" ]]; then
-  eval "$(<~/.ssh/.ssh-agent.rc)" > /dev/null
+
+export SSH_AGENT_RC=~/.ssh/.ssh-agent.rc
+if [[ -z "$SSH_AGENT_PID" ]]; then
+  if [[ -f $SSH_AGENT_RC &&
+        $(sed -nr 's/echo Agent pid ([0-9]*);/\1/p' $SSH_AGENT_RC) \
+          != $(pgrep -u "$USER" ssh-agent) ||
+        ! (-f $SSH_AGENT_RC) ]] ; then
+    pkill -u "$USER" ssh-agent
+    ssh-agent > $SSH_AGENT_RC
+  fi
+  eval "$(<$SSH_AGENT_RC)" > /dev/null
 fi
 
 sshh() {
