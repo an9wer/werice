2866609378884af7e5bb408da65f359a7fe0925a
an9wer <an9wer@gmail.com>
2019-01-16 16:05:44 +0800

    Update prompt icon

--- .me/lib/prompt.sh
+++ .me/lib/prompt.sh
@@ -11,24 +11,24 @@ me_git_prompt() {
     local branch="HEAD detached at ${ME_PROMPT_MAGENTA}$(git rev-parse --short HEAD)" 
 
   (( $(git status -s -uno | wc -l) == 0 )) &&
-    local status="${ME_PROMPT_GREEN}✓" ||
-    local status="${ME_PROMPT_RED}✗"
+    local status="${ME_PROMPT_GREEN}:)" ||
+    local status="${ME_PROMPT_RED}:("
 
-  echo "(${ME_PROMPT_BOLD}${branch} ${status}${ME_PROMPT_END})"
+  echo "G ${ME_PROMPT_BOLD}${branch} ${status}${ME_PROMPT_END}"
 }
 
 ps1_command() {
-  PS1="${ME_PROMPT_YELLOW}┏─━${ME_PROMPT_END} "
+  PS1="${ME_PROMPT_BOLD}${ME_PROMPT_YELLOW}.--*=${ME_PROMPT_END} "
   PS1+="$(me_venv_prompt)"
   PS1+="${ME_PROMPT_BOLD}${ME_PROMPT_GREEN}\u@\h${ME_PROMPT_END} "
   PS1+="at ${ME_PROMPT_BOLD}${ME_PROMPT_RED}\t${ME_PROMPT_END} "
   PS1+="in ${ME_PROMPT_BOLD}${ME_PROMPT_BLUE}\w${ME_PROMPT_END} "
   PS1+="$(me_git_prompt)\n"
-  PS1+="${ME_PROMPT_YELLOW}┗─━ \$ ${ME_PROMPT_END}"
+  PS1+="${ME_PROMPT_BOLD}${ME_PROMPT_YELLOW}\\\`--*= \$ ${ME_PROMPT_END}"
 }
 
 # ps1
-PROMPT_COMMAND=$PROMPT_COMMAND$'\n''ps1_command;'
+PROMPT_COMMAND=$PROMPT_COMMAND$'\n'"ps1_command;"
 
 # ps2
-PS2="${ME_PROMPT_YELLOW}┗─━ > ${ME_PROMPT_END}"
+PS2="${ME_PROMPT_BOLD}${ME_PROMPT_YELLOW}\\\`--*= > ${ME_PROMPT_END}"
