b746437789328d5e0f2be8a041f4d6d3b7747797
an9wer <an9wer@gmail.com>
2019-09-25 10:16:46 +0800

    [bashrc] Add alias of date, xclip, doc

--- .bashrc.d/alias.sh
+++ .bashrc.d/alias.sh
@@ -1,19 +1,37 @@
 alias sourceb="source ~/.bashrc"
 
+# vim
 alias vimv="vim ~/.vimrc"
 alias vimb="vim ~/bashrc"
 alias vimt="vim ~/.tmux.conf"
 alias vimtest="vim --noplugin -N -u"
 
+# ls
 alias ls="ls --color=auto"
 alias ll="ls -lh"
 alias lsc="ls --color=always"
 alias llc="ll --color=always"
 
+# grep
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
+alias grepc="grep --color=always"
+alias egrepc="egrep --color=always"
+alias fgrepc="fgrep --color=always"
 
+# diff
 alias diff="diff --color=auto"
 
-alias todo="vim ~/Documents/notes/todo.wiki"
+# date
+alias dt="date '+%Y/%m/%d'"
+alias dtw="date '+%Y/%m/%d %a'"
+
+# xclip
+alias xcp="xclip -r -selection clipboard"
+
+# doc
+alias todoo="vim ~/Documents/notes/todo.wiki"
+alias todo="vim ~/Documents/an9wer.github.io/TODO.txt"
+alias funny="vim ~/Documents/an9wer.github.io/funny.txt"
+alias dict="vim ~/.config/ibus/rime/an9wer.dict.yaml"
