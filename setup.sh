b5c2888f3396ac4905d88065a1d7d1714005ce88
an9wer <an9wer@gmail.com>
2020-10-21 23:38:11 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -e
+
+backup-origin() {
+  local origin=$HOME/$1
+
+  # $origin does not exist or is a symbolic link
+  if [[ ! -a $origin || -h $origin ]]; then
+    return
+  fi
+
+  # $origin.orig does not exist
+  if [[ ! -f $origin.orig ]]; then
+    mv "$origin" "$origin".orig
+  else
+    echo "Backup error: '$origin.orig' already exists."
+    return 1
+  fi
+}
+
+install-file() {
+  local target=$(readlink -e "$1")
+  local link=$HOME/$1
+  local linkdir=$(dirname "$link")
+
+  mkdir -p "$linkdir"
+
+  # $link does not exist
+  if [[ ! -a $link ]]; then
+    ln -sfv "$target" "$link"
+  # $link exists and is a symnolic link
+  elif [[ -h $link ]]; then
+    if [[ $(readlink -e "$link") != $target ]]; then
+      ln -sfv "$target" "$link"
+    fi
+  else
+    echo "Install error: '$link' already exists and isn't a symbolic link."
+    return 1
+  fi
+}
+
+
+rices_to_install=(
+  .xinitrc
+  .Xmodmap
+  .tmux.conf
+  .gitconfig
+  .bash_profile
+  .bashrc
+  .bashrc.d/shopt.sh
+  .bashrc.d/history.sh
+  .bashrc.d/alias.sh
+  .bashrc.d/prompt.sh
+  .bashrc.d/typos.sh
+  .bashrc.d/less.sh
+  .bashrc.d/cd.sh
+  .config/ibus/rime/default.custom.yaml
+  .config/ibus/rime/double_pinyin.custom.yaml
+  .config/dunst/dunstrc
+)
+
+for rice in "${rices_to_install[@]}"; do
+  if [[ ! -f $rice ]]; then
+    echo "Install error: '$rice' is not a file."
+    return 1
+  fi
+
+  backup-origin "$rice"
+  install-file "$rice"
+done
