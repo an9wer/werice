7549ec622426bbf9aa7b0d816a7e2722a4d0edc0
Runney Wu <an9wer@gmail.com>
2021-12-29 22:59:39 +0800

    feat(qutebrowser): set background color of tabs

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -7,3 +7,11 @@ config.set('tabs.close_mouse_button', 'right')
 # Set background color of selected tabs
 config.set('colors.tabs.selected.odd.bg', '#005577')
 config.set('colors.tabs.selected.even.bg', '#005577')
+config.set('colors.tabs.pinned.selected.odd.bg', '#005577')
+config.set('colors.tabs.pinned.selected.even.bg', '#005577')
+
+# Set backgound color of unselected tabs
+config.set('colors.tabs.odd.bg', 'grey')
+config.set('colors.tabs.even.bg', 'grey')
+config.set('colors.tabs.pinned.odd.bg', 'darkseagreen')
+config.set('colors.tabs.pinned.even.bg', 'darkseagreen')
