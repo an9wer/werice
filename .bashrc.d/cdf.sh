02b837c9589f3aab2de774e0cb527de7a755ecf0
Runney Wu <an9wer@gmail.com>
2023-08-18 23:15:18 +0930

    Rename cd.sh to cdf.sh

--- .bashrc.d/cdf.sh
+++ .bashrc.d/cdf.sh
@@ -1,8 +1,10 @@
+shopt -s globstar
+
 cdf() {
   declare -a dirs
   declare -i choice
 
-  # Only directory is acceptable
+  # filter out all directories
   for dir in "$@"; do
     if [[ -d $dir ]]; then
       dirs+=("$dir")
@@ -18,13 +20,9 @@ cdf() {
       printf "%s.\t%s\n" "$(( $idx + 1))" "${dirs[$idx]}"
     done
     while true; do
-      read -e -p "Which one is your choice? (1-${#dirs[@]}) " choice
+      read -e -p "Which one do you want to move into? (1-${#dirs[@]}) " choice
 
-      if (( $choice == 0 )); then
-        echo "Choice cannot be number 0, alpha or empty"
-      elif (( $choice > ${#dirs[@]} )); then
-        echo "Choice must be small than" ${#dirs[@]}
-      else
+      if (( $choice >= 1 )) && (( $choice <= ${#dirs[@]} )); then
         cd "${dirs[$choice - 1]}"
         break
       fi
