268c2dd90b1bedd4e387b2fa76f8e79a79a6f229
Runney Wu <an9wer@gmail.com>
2023-09-04 10:20:16 +0930

    bash: ignore fg in command history

--- .bashrc.d/history.sh
+++ .bashrc.d/history.sh
@@ -3,6 +3,6 @@ export HISTSIZE=-1      # unlimited
 export HISTFILESIZE=-1  # unlimited
 export HISTTIMEFORMAT="[%Y/%m/%d %T]  "
 export HISTCONTROL=ignoredups
-export HISTIGNORE="ls:pwd:vim:history:git status:xcplc"
+export HISTIGNORE="ls:fg:pwd:vim:history:git status:xcplc"
 
 shopt -s histappend
